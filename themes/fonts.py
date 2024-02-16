c25d17363e5fa244c7b13d8961cefd00a5c7f1f8
Nate Beversluis <nathanielbeversluis@gmail.com>
2024-02-16 01:50:47 +0100

    add additional colors and font options

--- themes/fonts.py
+++ themes/fonts.py
@@ -1,9 +1,9 @@
 from objc_util import ObjCClass
 
 # examples
-
 fonts = {
 	'Courier New': ObjCClass('UIFont').fontWithName_size_('Courier New', 12),
-	'Courier New Bold': ObjCClass('UIFont').fontWithName_size_('Courier New Bold', 12)
-		
+	'Courier New Bold': ObjCClass('UIFont').fontWithName_size_('Courier New Bold', 12),
+	'Courier New Italic': ObjCClass('UIFont').fontWithName_size_('Courier New Italic', 12),
+	'Courier New Bold Italic': ObjCClass('UIFont').fontWithName_size_('Courier New Bold Italic', 12)
 }
